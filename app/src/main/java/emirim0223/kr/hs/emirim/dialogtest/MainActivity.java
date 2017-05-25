2970b0bd6796d2b08062108f9a419752bce3e3a9
every-sushi <nadia0515@naver.com>
2017-05-25 20:44:22 +0900

    #2 Create Dialog

--- app/src/main/java/emirim0223/kr/hs/emirim/dialogtest/MainActivity.java
+++ app/src/main/java/emirim0223/kr/hs/emirim/dialogtest/MainActivity.java
@@ -1,12 +1,13 @@
 package emirim0223.kr.hs.emirim.dialogtest;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    String[] itemArr = {"진영","신우","산들","바로","공찬"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +18,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("B1A4");
+        dialog.setIcon(R.mipmap.ic_launcher);
+        //dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
+        dialog.setItems(itemArr,null);
+        dialog.setPositiveButton("OK",null);
+        dialog.show();
     }
 }
