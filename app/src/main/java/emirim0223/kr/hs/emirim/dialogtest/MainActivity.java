55dbd01e66ba68dc41f4829d42ebf1be00060b1e
every-sushi <nadia0515@naver.com>
2017-05-25 20:59:34 +0900

    #3 Change Dialog and Dialog Event Programming

--- app/src/main/java/emirim0223/kr/hs/emirim/dialogtest/MainActivity.java
+++ app/src/main/java/emirim0223/kr/hs/emirim/dialogtest/MainActivity.java
@@ -1,6 +1,7 @@
 package emirim0223.kr.hs.emirim.dialogtest;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +9,12 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     String[] itemArr = {"진영","신우","산들","바로","공찬"};
+    Button butDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button butDialog = (Button) findViewById(R.id.but_dialog);
+        butDialog = (Button) findViewById(R.id.but_dialog);
         butDialog.setOnClickListener(this);//현재 클래스가 handler클래스 자격을 가지고 있으므로 this(본인을 가르킴)를 사용
     }
 
@@ -22,7 +24,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dialog.setTitle("B1A4");
         dialog.setIcon(R.mipmap.ic_launcher);
         //dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
-        dialog.setItems(itemArr,null);
+        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                butDialog.setText(itemArr[i]);
+            }
+        });
         dialog.setPositiveButton("OK",null);
         dialog.show();
     }
